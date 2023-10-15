48f8ed0a2b24f7d5345944a35fd418ef777ed779
Yoshiyuki Kato <kattoyoshi@gmail.com>
2023-10-15 17:41:40 +0900

    chore: Update image name

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t pytorch-infinity:latest -f docker/Dockerfile . --build-arg USERNAME=$LOGNAME --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g)
+docker build -t mmIntegrated:latest -f docker/Dockerfile . --build-arg USERNAME=$LOGNAME --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g)
